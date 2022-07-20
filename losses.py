8a56105971b142a940e44adc82901b41dfc84698
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2022-07-20 15:44:02 +0200

    Update losses.py

--- losses.py
+++ losses.py
@@ -2,3 +2,20 @@ from tensorflow.keras import backend as K
 import numpy as np
 import tensorflow as tf
 
+smooth = 1.
+
+
+def average_dice_coef(y_true, y_pred):
+    loss = 0
+    label_length = y_pred.get_shape().as_list()[-1]
+    print(label_length)
+    for num_label in range(label_length):
+        y_true_f = K.flatten(y_true[..., num_label])
+        y_pred_f = K.flatten(y_pred[..., num_label])
+        intersection = K.sum(y_true_f * y_pred_f)
+        loss += (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return loss / label_length
+
+
+def average_dice_coef_loss(y_true, y_pred):
+    return -average_dice_coef(y_true, y_pred)
